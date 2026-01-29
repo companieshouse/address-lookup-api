c7806b5c32bf0296861646325cd760b73a913614
kinpang-CH <kpang@companieshouse.gov.uk>
2026-01-29 16:51:22 +0000

    Address PR comments an add PR template
    * Add template to be used for future pull requests
    * Remove references to other load balancers in data.tf
    * Remove address-lookup-api from healthcheck base path
    * Relocate itest to sit under correct directory

--- terraform/groups/ecs-service/data.tf
+++ terraform/groups/ecs-service/data.tf
@@ -27,24 +27,6 @@ data "aws_subnets" "application" {
   }
 }
 
-data "aws_lb" "service_lb" {
-  name = "${var.environment}-chs-apichgovuk"
-}
-
-data "aws_lb_listener" "service_lb_listener" {
-  load_balancer_arn = data.aws_lb.service_lb.arn
-  port              = 443
-}
-
-data "aws_lb" "secondary_lb" {
-  name = "${var.environment}-chs-apichgovuk-private"
-}
-
-data "aws_lb_listener" "secondary_lb_listener" {
-  load_balancer_arn = data.aws_lb.secondary_lb.arn
-  port              = 443
-}
-
 data "aws_lb" "internal_lb" {
   name = "${var.environment}-chs-internalapi"
 }

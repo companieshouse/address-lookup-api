bb3a5e83d75f4c982241ce6ea8ca7d201aa8fef7
imclaughlin-ch <imclaughlin@companieshouse.gov.uk>
2026-09-18 11:57:20 +0100

    ALS-43 - lamda modules

--- address-lookup-api/src/main/java/uk/gov/companieshouse/addresslookup/mapper/LegacyAddressMapper.java
+++ address-lookup-api/src/main/java/uk/gov/companieshouse/addresslookup/mapper/LegacyAddressMapper.java
@@ -25,10 +25,6 @@ public interface LegacyAddressMapper {
 
     default String toPremise(RoyalMailAddressLookup address) {
         return join(
-                address.getOrganisationName(),
-                address.getDepartmentName(),
-                address.getSubBuildingName(),
-                address.getBuildingName(),
                 address.getBuildingNumber() == null ? null : address.getBuildingNumber().toString());
     }
 

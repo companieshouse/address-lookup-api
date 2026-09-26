b9d9d48145617418be2e8b1fd738371b52d2abc1
sijegbulem-ch <sijegbulem@companieshouse.gov.uk>
2026-09-26 01:39:01 +0100

    fix for pom issue

--- terraform/groups/lambda/outputs.tf
+++
@@ -1,4 +0,0 @@
-output "liquibase_function_name" {
-  description = "The function Concourse invokes to apply a released db-schema changelog to Aurora"
-  value       = module.liquibase_lambda.lambda_function_name
-}
